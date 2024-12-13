70b620612a8920efa1dc5cd9004e3a5d2cf3b36c
Thomas Noone <tnoone@lcs.on.ca>
2024-12-13 15:13:39 -0500

    Made a C shape by accident

---
+++ CustonShapes/Shapes/CShape.swift
@@ -0,0 +1,34 @@
+//
+//  E Shape.swift
+//  CustonShapes
+//
+//  Created by Thomas Noone on 2024-12-13.
+//
+
+import SwiftUI
+
+
+struct Cshape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY * 0.33))
+        path.addLine(to: CGPoint(x: rect.maxX * 0.25, y: rect.maxY * 0.33))
+        path.addLine(to: CGPoint(x: rect.maxX * 0.25, y: rect.maxY * 0.66))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY * 0.66))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY * 0.99))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY * 0.99))
+
+
+
+
+
+        
+        return path
+    }
+}
+
+#Preview {
+    Cshape()
+}
