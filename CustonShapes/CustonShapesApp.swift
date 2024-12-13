f5c163a30c583a28c6caaddf3f1769fb046eac05
Thomas Noone <tnoone@lcs.on.ca>
2024-12-13 14:25:35 -0500

    Initial Commit

---
+++ CustonShapes/CustonShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustonShapesApp.swift
+//  CustonShapes
+//
+//  Created by Thomas Noone on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct CustonShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
